d1a0613a49b0d58f5cff132ec81335ee75608885
Zach2039 <sumyunguy2007@gmail.com>
2023-05-25 07:49:31 -0500

    More work; relicense to CC-BY

---
+++ src/ModUtil/EnumRailDirection.cs
@@ -0,0 +1,16 @@
+namespace VintageMinecarts.ModUtil
+{
+    public enum EnumRailDirection
+    {
+        EAST_TO_SOUTH,
+        SOUTH_TO_WEST,
+        WEST_TO_NORTH,
+        NORTH_TO_EAST,
+        NORTH_TO_SOUTH,
+        WEST_TO_EAST,
+        UPWARDS_NORTH,
+        UPWARDS_SOUTH,
+        UPWARDS_WEST,
+        UPWARDS_EAST
+    }
+}
